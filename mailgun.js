b7da92696e71de08b4819ab0a05c6ed48acdb195
dudesl <dudesl@gmail.com>
2015-03-05 17:11:10 -0300

    the first try for a CLI tool

--- mailgun.js
+++ mailgun.js
@@ -1,18 +1,18 @@
 var http = require('http');
 var mailgun = require('mailgun-js')({
-  // apiKey 'key-f1d1f5a41e5528beaeb63f35b6a7c80c',
-  // domain 'sandboxd2cba16debb04c3a9d37570b82f0c884.mailgun.org'
-  apiKey: 'key',
-  domain: 'sandbox'
+  apiKey: 'key-f1d1f5a41e5528beaeb63f35b6a7c80c',
+  domain: 'sandboxd2cba16debb04c3a9d37570b82f0c884.mailgun.org'
+  // apiKey: 'key',
+  // domain: 'sandbox'
 });
 
-var url = '//';
+var url = 'http://localhost:3000';
 var body = '';
 
 var data = {
   from: 'Mailgun Postmaster <postmaster@sandboxd2cba16debb04c3a9d37570b82f0c884.mailgun.org>',
-  to: '',
-  subject: 'Yet Another Subject'
+  to: 'santiago.barchetta@mercadolibre.com, 80722a8@emailtests.com',
+  subject: '[TEST BR] Muito em breve os seus compradores poderão pagar com 2 cartões de crédito'
 }
 
 http.get(url, function(res) {
